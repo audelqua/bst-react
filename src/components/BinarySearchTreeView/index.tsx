c1d0a0799271807b5eb7056c47ff092c3baa496f
audrey <mbozorgi604@gmail.com>
2023-03-10 20:31:33 +0330

    painting tree in progress

--- src/components/BinarySearchTreeView/index.tsx
+++ src/components/BinarySearchTreeView/index.tsx
@@ -2,20 +2,25 @@
 import { useLayoutEffect, useState } from 'react'
 import { useBinaryTreeGenerator } from 'src/hooks/useBinaryTreeGenerator'
 import { useBinarySearchTreeContext } from 'src/hooks/useBinarySearchTreeContext'
+import BinaryNode from 'src/components/BinaryNode'
+import styles from './BinarySearchTreeView.module.css'
 
 const BinarySearchTreeView = () => {
     const { handleBinaryTreeGenerator } = useBinaryTreeGenerator()
-    const [ _, bstIngredients ] = useBinarySearchTreeContext()
+    const [ ,bstIngredients ] = useBinarySearchTreeContext()
     const [bstNodes, setBstNodes] = useState<any>()
-
+    console.log('bstNodes', bstNodes);
+    
     useLayoutEffect(() => {
         const newTree = handleBinaryTreeGenerator(bstIngredients)
         setBstNodes(newTree)
     }, [bstIngredients])
     
     return (
-        <div>
-            
+        <div className={styles['tree-wrapper']}>
+            <BinaryNode />
+            <BinaryNode />
+            <BinaryNode />
         </div>
     )
 }
